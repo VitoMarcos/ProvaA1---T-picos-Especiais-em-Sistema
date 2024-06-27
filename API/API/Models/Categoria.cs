e91c3a2fed410c229bc38b963334a4c7b37b7b82
VitoMarcos <163423080+VitoMarcos@users.noreply.github.com>
2024-06-27 11:39:13 -0300

    Delete API directory

--- API/API/Models/Categoria.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Models;
-
-public class Categoria
-{
-    public string CategoriaId { get; set; } = Guid.NewGuid().ToString();
-    public string? Nome { get; set; }
-    public DateTime CriadoEm { get; set; } = DateTime.Now;
-}
